2af29478e188db440d9d5e7577035027a35adb4f
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2022-10-28 23:50:38 +0400

    fixed issues

--- src/context.js
+++ src/context.js
@@ -11,7 +11,68 @@ const urlByName = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
+  const [loading, setLoading] = useState(false);
+  const [searchedValue, setSearchedValue] = useState('');
+  const [cocktails, setCocktails] = useState([]);
+  // const [error, setError] = useState(false);
 
+  const fetchCocks = useCallback(async () => {
+    setLoading(true);
+    try {
+      // setSearchedValue('az');
+      const res = await fetch(`${urlByName} ${searchedValue}`);
+      const data = await res.json();
+      const { drinks } = data;
+      // console.log(drinks);
+
+      if (drinks) {
+        const newCockList = drinks.map(cocktail => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+            cocktail;
+
+          return {
+            cockID: idDrink,
+            cockName: strDrink,
+            cockImg: strDrinkThumb,
+            cockInfo: strAlcoholic,
+            cockGlass: strGlass,
+          };
+
+          /*
+        const {
+          idDrink: cockID,
+          strDrink: cockName,
+          strDrinkThumb: cockImg,
+          strAlcoholic: cockInfo,
+          strGlass: cockGlass,
+        } = cocktail;
+
+        return {
+          cockID,
+          cockName,
+          cockImg,
+          cockInfo,
+          cockGlass,
+        };
+        */
+        });
+        // console.log(cocktails);
+        setCocktails(newCockList);
+      } else {
+        setCocktails([]);
+      }
+      setLoading(false);
+    } catch (err) {
+      console.log(err.message);
+      setLoading(false);
+    }
+    // console.log(drinks);
+  }, [searchedValue]);
+
+  useEffect(() => {
+    fetchCocks();
+  }, [searchedValue]);
+  // }, [searchedValue, fetchCocks]);
 
   return (
     <AppContext.Provider
@@ -35,67 +96,3 @@ export const useGlobalContext = () => {
 };
 
 export { AppContext, AppProvider };
-
-
-// const [loading, setLoading] = useState(false);
-// const [searchedValue, setSearchedValue] = useState('');
-// const [cocktails, setCocktails] = useState([]);
-// // const [error, setError] = useState(false);
-
-// const fetchCocks = useCallback(async () => {
-//   setLoading(true);
-//   try {
-//     // setSearchedValue('az');
-//     const res = await fetch(`${urlByName} ${searchedValue}`);
-//     const data = await res.json();
-//     const { drinks } = data;
-//     // console.log(drinks);
-
-//     if (drinks) {
-//       const newCockList = drinks.map(cocktail => {
-//         const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
-//           cocktail;
-
-//         return {
-//           cockID: idDrink,
-//           cockName: strDrink,
-//           cockImg: strDrinkThumb,
-//           cockInfo: strAlcoholic,
-//           cockGlass: strGlass,
-//         };
-
-//         /*
-//         const {
-//           idDrink: cockID,
-//           strDrink: cockName,
-//           strDrinkThumb: cockImg,
-//           strAlcoholic: cockInfo,
-//           strGlass: cockGlass,
-//         } = cocktail;
-
-//         return {
-//           cockID,
-//           cockName,
-//           cockImg,
-//           cockInfo,
-//           cockGlass,
-//         };
-//         */
-//       });
-//       // console.log(cocktails);
-//       setCocktails(newCockList);
-//     } else {
-//       setCocktails([]);
-//     }
-//     setLoading(false);
-//   } catch (err) {
-//     console.log(err.message);
-//     setLoading(false);
-//   }
-//   // console.log(drinks);
-// }, [searchedValue]);
-
-// useEffect(() => {
-//   fetchCocks();
-// }, [searchedValue]);
-// // }, [searchedValue, fetchCocks]);
